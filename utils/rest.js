a4768866649af2c19f4a7560f86569edfbabbae7
Lé <elcasaboxi@gmail.com>
2023-12-16 21:09:58 +0800

    update axios and rest

--- utils/rest.js
+++ utils/rest.js
@@ -4,17 +4,37 @@ const axios = require(`axios`);
 // const OLLESOCKET_API_KEY = process.env.OLLESOCKET_API_KEY;
 
 module.exports = {
-  get: async (url) => {
-    return (await axios.get(url)).data;
+  get: async (d) => {
+    let headers = d.headers || {};
+    // if (d.url.includes(OLLESOCKET_API_URL)) {
+    //   payload.api_key = OLLESOCKET_API_KEY;
+    // }
+    let res = null;
+    
+    if (headers) {
+      res = (await axios.get(d.url || ``, { headers }));
+    } else {
+      res = (await axios.get(d.url || ``));
+    }
+
+    return d.all ? res : res.data;
   },
 
   post: async (d) => {
     try {
+      let headers = d.headers || {};
       let payload = d.payload || {};
       // if (d.url.includes(OLLESOCKET_API_URL)) {
       //   payload.api_key = OLLESOCKET_API_KEY;
       // }
-      let res = (await axios.post(d.url || ``, payload));
+      let res = null;
+
+      if (headers) {
+        res = (await axios.post(d.url || ``, payload, { headers }));
+      } else {
+        res = (await axios.post(d.url || ``, payload));
+      }
+
       return d.all ? res : res.data;
     } catch (e) {
       console.log(e);
